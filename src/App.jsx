e9564fde552ba28227a17101d84740756000562c
Seydina Limamou Laye Yade <109959995+seydinalimamoulayeyade@users.noreply.github.com>
2026-04-19 04:49:32 +0100

    feat: brancher la HomePage comme page principale

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import AjouterProjet from "./components/AjouterProjet";
 import DetaillerProjet from "./components/DetaillerProjet";
 import Dossier from "./components/Dossier";
 import Layout from "./components/Layout";
+import HomePage from "./components/HomePage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Dossier />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projets" element={<Dossier />} />
         <Route path="/ajouter" element={<AjouterProjet />} />
         <Route path="/modifier/:id" element={<AjouterProjet />} />
         <Route path="/projets/:id" element={<DetaillerProjet />} />
