52e20cca849e735e0629f48994bc77075bdf555c
Seydina Limamou Laye Yade <109959995+seydinalimamoulayeyade@users.noreply.github.com>
2026-04-20 13:14:33 +0100

    feat: créer serveur express principal

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./src/config/db');
+const projectRoutes = require('./src/routes/projectRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB connection
+connectDB();
+
+// Routes
+app.use('/api/projets', projectRoutes);
+
+// Health check
+app.get('/', (req, res) => {
+  res.send('API backend fonctionnelle 🚀');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
